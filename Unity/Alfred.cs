a5324a27eb9f28c1ec695f1240e27fd4802d7ae0
Robert Konrad <robert@ktx-software.com>
2016-01-25 12:47:24 +0100

    Added C# bridge code.

---
+++ Unity/Alfred.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Alfred {
+	private static AndroidJavaClass clazz;
+
+	public static void Init() {
+		if (Application.platform == RuntimePlatform.Android) {
+			clazz = new AndroidJavaClass("de.tu_darmstadt.kom.alfredplugin.Alfred");
+			clazz.CallStatic("init");
+		}
+	}
+
+	public static int GetNextAction() {
+		if (Application.platform == RuntimePlatform.Android) {
+			return clazz.CallStatic<int>("getNextAction");
+		}
+		else {
+			return -1;
+		}
+	}
+}
